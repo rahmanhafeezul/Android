c4b577b889a7f2fed60e21abb525e8f49f2f47b6
Rahman <rahman.hafeezul@gmail.com>
2013-04-28 00:36:37 +0530

    Android game implemented

---
+++ android_game/Sensors.java
@@ -0,0 +1,20 @@
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ListView;
+
+/**
+* Shows a list of all Sensors on this phone.
+*
+* 
+*/
+public class Sensors extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sensors);
+        
+ListView sensorList = (ListView) findViewById(R.id.sensor_list);
+sensorList.setAdapter(new SensorListAdapter(this));
+    }
+}
